0c619140f71a1c15c8297e9f5975df8a161e337b
Sergei Viniarskii <sergei@viniarskii.com>
2025-12-24 13:04:42 +0100

    Removed horizontal insets

--- composeApp/src/commonMain/kotlin/com/viniarskii/jbtest/App.kt
+++ composeApp/src/commonMain/kotlin/com/viniarskii/jbtest/App.kt
@@ -2,6 +2,8 @@ package com.viniarskii.jbtest
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +18,13 @@ fun App() {
     MaterialTheme(colorScheme = lightScheme) {
         Box(
             modifier = Modifier
-                .windowInsetsPadding(WindowInsets.safeContent)
+                .windowInsetsPadding(
+                    WindowInsets.safeContent
+                        // Okay, let's ignore horizontal insets
+                        // because it doesn't look nice.
+                        // In reality, it need to be covered in design.
+                        .only(WindowInsetsSides.Vertical)
+                )
         ) {
             MainScreen()
         }
